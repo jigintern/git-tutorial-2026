6b42bcb3ac8dd8bc3b8cd60aad57bb179f4da5ab
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2026-08-18 11:19:13 +0900

    hello worldたくさん

--- miyake.py
+++ miyake.py
@@ -1,1 +1,25 @@
-print("Hello World")
+def a():
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+
+def b():
+    a()
+    a()
+    a()
+    a()
+    
+def c():
+    b()
+    b()
+    b()
+    b()
+
+def d():
+    c()
+    c()
+    c()
+    c()
+
+d()
